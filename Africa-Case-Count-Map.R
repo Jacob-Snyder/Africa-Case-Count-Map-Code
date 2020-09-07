716e7b886fb91f2dfbafbfbac4374962923dae4a
Jacob-Snyder <GitHubEmail@something.com>
2020-09-06 22:00:49 -0400

    copy and paste

---
+++ Africa-Case-Count-Map.R
@@ -0,0 +1,77 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+install.packages("sf")
+library(sf)
+library(shiny)
+library(leaflet)
+library(spData)
+library(dplyr)
+
+# Define UI for application that filters map points based on year and minimum population
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Global Population Growth"),
+  
+  # Sidebar with a slider input for year, numeric input for population 
+  sidebarLayout(
+    sidebarPanel(
+      
+      sliderInput("year",
+                  "Year",
+                  min = 1960,
+                  max = 2035,
+                  step = 5,
+                  sep = "",
+                  value = 1960),
+      
+      numericInput("pop_min",
+                   "Minimum Population (in millions)",
+                   min = 0,
+                   max = 20,
+                   value = 10)
+    ),
+    
+    # Show the map and table
+    mainPanel(
+      # plotOutput("distPlot"),
+      leafletOutput("map"),
+      dataTableOutput("table")
+    )
+  )
+)
+
+# Define server logic required to draw a map and table
+server <- function(input, output) {
+  
+  
+  output$map <- renderLeaflet({
+    
+    pop_by_year <- filter(urban_agglomerations, 
+                          year == input$year,
+                          population_millions > input$pop_min)
+    
+    leaflet(data = pop_by_year) %>%
+      addTiles() %>%
+      addMarkers()
+  })
+  
+  output$table <- renderDataTable({
+    
+    pop_by_year <- filter(urban_agglomerations, 
+                          year == input$year,
+                          population_millions > input$pop_min)
+    
+    pop_by_year
+    
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
