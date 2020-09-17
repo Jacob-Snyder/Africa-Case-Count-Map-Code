8fc6e98742426a2a0d2a3e95643d70a3a3e575e8
Jacob-Snyder <GitHubEmail@something.com>
2020-09-17 10:10:48 -0400

    Adding code; modifying code to try to incorporate my modified case count dataset (line 31)

--- World Border Chloropleth.R
+++ World Border Chloropleth.R
@@ -19,3 +19,32 @@ library(leaflet)
 mypalette <- colorNumeric(palette = "viridis", domain = world_spdf@data$POP2005, na.color = "transparent")
 mypalette(c(45,43))
 
+# Basic choropleth with leaflet?
+m <- leaflet(world_spdf) %>% 
+  addTiles()  %>% 
+  setView( lat=10, lng=0 , zoom=2) %>%
+  addPolygons( fillColor = ~mypalette(POP2005), stroke=FALSE )
+
+m
+
+cbind(data,CaseCount="1:246")
+world_spdf$data <- "Modified Africa Case Count Data"
+
+#using a histogram to check distribution of variable 
+#and understand which color scale should be used
+
+# load ggplot2
+library(ggplot2)
+
+# Distribution of the population per country?
+world_spdf@data %>% 
+  ggplot( aes(x=as.numeric(POP2005))) + 
+  geom_histogram(bins=20, fill='#69b3a2', color='white') +
+  xlab("Population (M)") + 
+  theme_bw()
+
+Africa_Case_Count_Data@data %>%
+  ggplot(aes(x = as.numeric(Case_Count))) +
+  geom_histogram(bins = 20, fill='#69b3a2', color="white")+
+  xlab("Case Count") + 
+  theme_bw()
