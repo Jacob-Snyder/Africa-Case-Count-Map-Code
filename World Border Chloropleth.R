4ffcc32dae5155926b34f2e436bb884bdad3e081
Jacob-Snyder <GitHubEmail@something.com>
2020-09-17 10:13:14 -0400

    Changing all of POP2005 to casecount

--- World Border Chloropleth.R
+++ World Border Chloropleth.R
@@ -11,24 +11,24 @@ world_spdf <- shapefile("TM_WORLD_BORDERS_SIMPL-0.3.shp")
 #cleaning data
 #creating spatial polygon data frame
 library(dplyr)
-world_spdf@data$POP2005[ which(world_spdf@data$POP2005 == 0)] = NA
-world_spdf@data$POP2005 <- as.numeric(as.character(world_spdf@data$POP2005)) / 1000000 %>% round(2)
+world_spdf@data$casecount[ which(world_spdf@data$casecount == 0)] = NA
+world_spdf@data$casecount <- as.numeric(as.character(world_spdf@data$casecount)) / 1000000 %>% round(2)
 
 #creating a color paletter for the map
 library(leaflet)
-mypalette <- colorNumeric(palette = "viridis", domain = world_spdf@data$POP2005, na.color = "transparent")
+mypalette <- colorNumeric(palette = "viridis", domain = world_spdf@data$casecount, na.color = "transparent")
 mypalette(c(45,43))
 
 # Basic choropleth with leaflet?
 m <- leaflet(world_spdf) %>% 
   addTiles()  %>% 
   setView( lat=10, lng=0 , zoom=2) %>%
-  addPolygons( fillColor = ~mypalette(POP2005), stroke=FALSE )
+  addPolygons( fillColor = ~mypalette(casecount), stroke=FALSE )
 
 m
 
-cbind(data,CaseCount="1:246")
-world_spdf$data <- "Modified Africa Case Count Data"
+#adding "data" to world_spdf
+world_spdf$casecount <- "Modified Africa Case Count Data"
 
 #using a histogram to check distribution of variable 
 #and understand which color scale should be used
